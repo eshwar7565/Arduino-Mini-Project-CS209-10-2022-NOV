0ad1059ce1a358bb99d7e9187fca3b919b2158c5
eshwar7565 <110085174+eshwar7565@users.noreply.github.com>
2022-12-01 02:02:36 +0530

    Add files via upload

---
+++ CODE/ARDUINO_OBSTACLE_AVOIDING_CAR/ARDUINO_OBSTACLE_AVOIDING_CAR.ino
@@ -0,0 +1,185 @@
+//ARDUINO OBSTACLE AVOIDING CAR//
+
+
+/* Name - Rachakonda Eshwar 
+ *  Roll - 2101AI25
+ *  Email- eshwar_2101ai25@iitp.ac.in
+ */
+// Before uploading the code include the necessary libraries//
+//AFMotor Library
+//NewPing Library 
+//Servo Library 
+// To Install the libraries go to sketch >> Include Library >> Add .ZIP File >> 
+//Select the Downloaded ZIP files//
+
+
+#include <AFMotor.h>  
+#include <NewPing.h>
+#include <Servo.h> 
+
+#define TRIG_PIN A4 
+#define ECHO_PIN A5 
+#define MAX_DISTANCE 200 
+#define MAX_SPEED 190 // sets speed of DC  motors
+#define MAX_SPEED_OFFSET 20
+
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE); 
+
+AF_DCMotor motor1(1, MOTOR12_1KHZ); 
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ);
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+Servo myservo;   
+
+boolean goesForward=false;
+int distance = 100;
+int speedSet = 0;
+
+void setup() {
+
+  myservo.attach(10);  
+  myservo.write(65); 
+  delay(2000);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+}
+
+void loop() {
+ int distanceR = 0;
+ int distanceL =  0;
+ delay(40);
+ 
+ if(distance<=30)
+ {
+  moveStop();
+  delay(300);
+  moveBackward();
+  delay(100);
+  moveStop();
+  delay(300);
+  distanceR = lookRight();
+  delay(200);
+  distanceL = lookLeft();
+  delay(200);
+
+  if(distanceR>=distanceL)
+  {
+    turnRight();
+    moveStop();
+  }else
+  {
+    
+    turnLeft();
+
+    moveStop();
+  }
+ }else
+ {
+  moveForward();
+ }
+ distance = readPing();
+}
+
+int lookRight()
+{
+    myservo.write(0); 
+    delay(500);
+    int distance = readPing();
+    delay(100);
+    myservo.write(65); 
+    return distance;
+}
+
+int lookLeft()
+{
+    myservo.write(130); 
+    delay(500);
+    int distance = readPing();
+    delay(100);
+    myservo.write(65); 
+    return distance;
+    delay(100);
+}
+
+int readPing() { 
+  delay(70);
+  int cm = sonar.ping_cm();
+  if(cm==0)
+  {
+    cm = 250;
+  }
+  return cm;
+}
+
+void moveStop() {
+  motor1.run(RELEASE); 
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+  } 
+  
+void moveForward() {
+
+ if(!goesForward)
+  {
+    goesForward=true;
+    motor1.run(FORWARD);      
+    motor2.run(FORWARD);
+    motor3.run(FORWARD); 
+    motor4.run(FORWARD);     
+   for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
+   {
+    motor1.setSpeed(speedSet);
+    motor2.setSpeed(speedSet);
+    motor3.setSpeed(speedSet);
+    motor4.setSpeed(speedSet);
+    delay(5);
+   }
+  }
+}
+
+void moveBackward() {
+    goesForward=false;
+    motor1.run(BACKWARD);      
+    motor2.run(BACKWARD);
+    motor3.run(BACKWARD);
+    motor4.run(BACKWARD);  
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
+  {
+    motor1.setSpeed(speedSet);
+    motor2.setSpeed(speedSet);
+    motor3.setSpeed(speedSet);
+    motor4.setSpeed(speedSet);
+    delay(5);
+  }
+}  
+
+void turnRight() {
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);     
+  delay(1000);
+  motor1.run(FORWARD);      
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);      
+} 
+ 
+void turnLeft() {
+  motor1.run(BACKWARD);     
+  motor2.run(BACKWARD);  
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);   
+  delay(1000);
+  motor1.run(FORWARD);     
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+}  
